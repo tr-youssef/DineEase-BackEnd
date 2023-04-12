91910b2105cc7c98f63b8d5e22233b1a22180722
tr.youssef <tr.youssef@gmail.com>
2023-04-11 23:06:24 -0600

    adding a test call

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5001;
