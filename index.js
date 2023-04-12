920ac2f5ced18a025cb66865f86ecc220c53f2f2
tr.youssef <tr.youssef@gmail.com>
2023-04-11 21:41:20 -0600

    create express server

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+  .catch((error) => console.log(error));
