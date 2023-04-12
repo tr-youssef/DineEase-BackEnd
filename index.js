a8e7e1d5ca131758dc646936ce63ad2cbd066242
tr.youssef <tr.youssef@gmail.com>
2023-04-11 22:06:40 -0600

    connect to database + adding user model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
---
+++ models/users.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
