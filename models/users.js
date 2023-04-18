dd9c15c8dd9564e31dc42793082d433e977cf455
Mousumi Saha <mousumisaha927@gmail.com>
2023-04-17 23:19:09 -0600

    modified userSchema

--- models/users.js
+++ models/users.js
@@ -4,7 +4,15 @@ const userSchema = mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
+  role: { type: String, require: true },
+  active: { type: Boolean, require: true },
   password: { type: String, required: true },
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    require: [true, "Restaurant is required"]
+  },
+
 });
 
 const User = mongoose.model("User", userSchema);
