3873429f2b48fe3e22d2a9673969c69ccd634976
Mousumi Saha <mousumisaha927@gmail.com>
2023-04-24 10:27:43 -0600

    merge to main

--- routes/tables.js
+++ routes/tables.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAvailableTablesByServerId } from "../controllers/tables.js";
+import { getAvailableTablesByServerId, addTable, getTableById, getTables } from "../controllers/tables.js";
 
 const router = express.Router();
 
 router.get("/availableTables/:serverId", getAvailableTablesByServerId);
+router.get("/:id", getTableById);
+router.post("/", addTable);
+router.get("/", getTables);
+// router.patch("/status/:id", updateStatusTable);
 
 export default router;
