359537b846e8341d7ee1752f974bb71705155489
Mousumi Saha <mousumisaha927@gmail.com>
2023-04-24 11:07:22 -0600

    add commit

--- routes/tables.js
+++ routes/tables.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getAvailableTablesByServerId, addTable, getTableById, getTables } from "../controllers/tables.js";
-
+import { addTable, getTableById, getTables }
+ from "../controllers/Tables.js";
 const router = express.Router();
 
-router.get("/availableTables/:serverId", getAvailableTablesByServerId);
 router.get("/:id", getTableById);
 router.post("/", addTable);
 router.get("/", getTables);
 // router.patch("/status/:id", updateStatusTable);
 
 export default router;
+
