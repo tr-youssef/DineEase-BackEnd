fd1aebdfefdf39b148444d30d11a778838f3671b
Mousumi Saha <mousumisaha927@gmail.com>
2023-04-24 09:29:52 -0600

    commit added

--- controllers/tables.js
+++ controllers/tables.js
@@ -6,35 +6,22 @@ export const getAvailableTablesByServerId = async (req, res) => {
   try {
     // TODO: Filter by server id later.
     const { serverId } = req.params;
-    // const token = req.headers.authorization.split(" ")[1];
-    // if (token) {
-    //   let decodedData = jwt.verify(token, process.env.PRIVATE_KEY);
-    //   req.userId = decodedData?.id;
-    //   req.restaurantId = decodedData?.restaurantId;
-    // }
-    const tables = await Tables.find({ status: "Available" });
-    if (!tables) {
+    const token = req.headers.authorization.split(" ")[1];
+    if (token) {
+      let decodedData = jwt.verify(token, process.env.PRIVATE_KEY);
+      req.userId = decodedData?.id;
+      req.restaurantId = decodedData?.restaurantId;
+      }
+      let tables = await Tables.find({ status: "Filled" });
+      const filteredTables = tables.filter(
+        (table) =>
+          table.restaurantId.toString() === req.restaurantId &&
+          table.userId.toString() === req.userId
+      );
+    if (!filteredTables) {
       res.status(404).send({ message: `No table found.` });
     } else {
-      const tableIds = tables.map((a) =>
-        a._id.toString().replace(/new ObjectId\(|\)/g, "")
-      );
-      const bookeds = await Bookeds.find({
-        tableId: { $in: tableIds },
-      });
-      const tablesWithBookingId = tables.map((table) => {
-        return {
-          nameOfTable: table.nameOfTable,
-          capacity: table.capacity,
-          bookingId: bookeds.find(
-            (booking) =>
-              booking.tableId ===
-              table._id.toString().replace(/new ObjectId\(|\)/g, "")
-          )?._id,
-        };
-      });
-
-      res.status(200).json(tablesWithBookingId);
+      res.status(200).json(filteredTables);
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
