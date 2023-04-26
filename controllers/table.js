895e0cba3c82ab8361d77d9877b2f580951a79fe
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-04-26 03:36:34 -0600

    addded a getFilledTables function - added deleteTable route - added filledTable route

--- controllers/table.js
+++ controllers/table.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import Table from "../models/table.js";
+import Booked from "../models/booked.js";
+
 
 export const getTableById = async (req, res) => {
   try {
@@ -139,11 +141,48 @@ export const getAvailableTables = async (req, res) => {
     }
     let tables = await Table.find({ status: "available" }).populate({ path: "userId" }).populate({ path: "restaurantId" });
     // const filteredTables = tables.filter((table) => table.restaurantId._id.toString() === req.restaurantId && table.userId._id.toString() === req.userId);
-    console.log('tables', tables)
     if (!tables) {
       res.status(404).send({ message: `No table found.` });
     } else {
       res.status(200).json(tables);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getFilledTables = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (token) {
+      let decodedData = jwt.verify(token, process.env.PRIVATE_KEY);
+      req.userId = decodedData?.id;
+      req.restaurantId = decodedData?.restaurantId;
+    }
+    let tables = await Booked.find({ status: "NewClient" })
+      .select({ _id: 1, bookedAt: 1 })
+      .populate({ path: "tableId", select: { restaurantId: 1, nameOfTable: 1, capacity: 1 }, populate: { path: "userId", select: { userId: 1 } } });
+
+    const formattedTables = tables.map((table) => {
+      const bookedAt = new Date(table.bookedAt);
+      const formattedDate = bookedAt.toLocaleDateString();
+      const formattedTime = bookedAt.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      const waitingTime = Math.round((new Date() - bookedAt) / 1000); 
+      let waitingTimeText;
+      if (waitingTime < 60) {
+        waitingTimeText = `${waitingTime} sec`;
+      } else if (waitingTime < 3600) {
+        waitingTimeText = `${Math.floor(waitingTime / 60)} min`;
+      } else {
+        waitingTimeText = `${Math.floor(waitingTime / 3600)} hr`;
+      }
+      return { _id: table._id, nameOfTable: table.tableId.nameOfTable, capacity: table.tableId.capacity, bookedAt: `${formattedDate} ${formattedTime}`, waitingTime: waitingTimeText };
+    });
+
+    if (!formattedTables) {
+      res.status(404).send({ message: `No table found.` });
+    } else {
+      res.status(200).json(formattedTables);
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
--- routes/table.js
+++ routes/table.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { addTable, getTableById, getTables, getAlreadyOrderedTablesByServerId, getAvailableTables, updateTable } from "../controllers/table.js";
+import { addTable, getTableById, getTables, getAlreadyOrderedTablesByServerId, getAvailableTables, updateTable, getFilledTables, deleteTable } from "../controllers/table.js";
 const router = express.Router();
 
 router.get("/getTable/:id", getTableById);
 router.post("/", addTable);
 router.get("/", getTables);
 router.patch("/status/:id", updateTable)
+router.delete("/:id", deleteTable)
 router.get("/alreadyOrdered", getAlreadyOrderedTablesByServerId);
 router.get("/availableTables", getAvailableTables)
+router.get("/filledTables", getFilledTables)
 
 export default router;
