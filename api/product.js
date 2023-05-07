774f28350e174a3e84015de099230223ba72625d
tr.youssef <tr.youssef@gmail.com>
2023-05-06 20:51:01 -0600

    test vercel

--- api/product.js
+++ api/product.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router();
 router.get("/", async (req, res) => {
   try {
+    console.log("here");
     res.json({ status: 200, message: "Get data has succesfully" });
   } catch (error) {
     console.log(error);
