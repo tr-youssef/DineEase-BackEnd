707e77d234f0b595427570be440049d185fa5d2c
tr.youssef <tr.youssef@gmail.com>
2023-05-07 04:42:02 -0600

    delete a function that was not used

--- controllers/table.js
+++ controllers/table.js
@@ -110,26 +110,6 @@ export const getTables = async (req, res) => {
   }
 };
 
-export const getAlreadyOrderedTablesByServerId = async (req, res) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    if (token) {
-      let decodedData = jwt.verify(token, process.env.PRIVATE_KEY);
-      req.userId = decodedData?.id;
-      req.restaurantId = decodedData?.restaurantId;
-    }
-    let tables = await Table.find({ status: "Filled" }).populate({ path: "userId" }).populate({ path: "restaurantId" });
-    const filteredTables = tables.filter((table) => table.restaurantId._id.toString() === req.restaurantId && table.userId._id.toString() === req.userId);
-    if (!filteredTables) {
-      res.status(404).send({ message: `No table found.` });
-    } else {
-      res.status(200).json(filteredTables);
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
 export const getAvailableTables = async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
@@ -162,26 +142,24 @@ export const getFilledTables = async (req, res) => {
       .select({ _id: 1, bookedAt: 1 })
       .populate({ path: "tableId", select: { restaurantId: 1, nameOfTable: 1, capacity: 1 }, populate: { path: "userId", select: { userId: 1 } } });
 
+    const bookedTables = await Booked.find({ status: "Payed" }).select({ bookedAt: 1, leavedAt: 1, tableId: 1 });
+    let totalWaitTime = 0;
+    let totalTables = 0;
+    for (let table of bookedTables) {
+      const bookedAt = new Date(table.bookedAt);
+      const leavedAt = new Date(table.leavedAt);
+      const waitTime = Math.round((leavedAt - bookedAt) / 1000);
+      totalWaitTime += waitTime;
+      totalTables++;
+    }
+    const averageWaitTime = Math.round(totalWaitTime / totalTables);
+
     const formattedTables = tables.map(async (table) => {
       const bookedAt = new Date(table.bookedAt);
       const formattedTime = bookedAt.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
-
-      const bookedTables = await Booked.find({ status: "Payed" }).select({ bookedAt: 1, leavedAt: 1, tableId: 1 });
-      let totalWaitTime = 0;
-      let totalTables = 0;
-      for (let table of bookedTables) {
-        const bookedAt = new Date(table.bookedAt);
-        const leavedAt = new Date(table.leavedAt);
-        const waitTime = Math.round((leavedAt - bookedAt) / 1000);
-        totalWaitTime += waitTime;
-        totalTables++;
-      }
-      const averageWaitTime = Math.round(totalWaitTime / totalTables);
-
       const waitingTimeInSeconds = bookedAt.getTime() + averageWaitTime * 1000;
       const waitingTime = new Date(waitingTimeInSeconds);
       const waitingTimeText = waitingTime.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
-
       return { _id: table._id, nameOfTable: table.tableId.nameOfTable, capacity: table.tableId.capacity, bookedAt: `${formattedTime}`, waitingTime: waitingTimeText };
     });
 
--- routes/table.js
+++ routes/table.js
@@ -1,14 +1,13 @@
 import express from "express";
-import { addTable, getTableById, getTables, getAlreadyOrderedTablesByServerId, getAvailableTables, updateTable, getFilledTables, deleteTable } from "../controllers/table.js";
+import { addTable, getTableById, getTables, getAvailableTables, updateTable, getFilledTables, deleteTable } from "../controllers/table.js";
 const router = express.Router();
 
 router.get("/getTable/:id", getTableById);
 router.post("/", addTable);
 router.get("/", getTables);
-router.patch("/status/:id", updateTable)
-router.delete("/:id", deleteTable)
-router.get("/alreadyOrdered", getAlreadyOrderedTablesByServerId);
-router.get("/availableTables", getAvailableTables)
-router.get("/filledTables", getFilledTables)
+router.patch("/status/:id", updateTable);
+router.delete("/:id", deleteTable);
+router.get("/availableTables", getAvailableTables);
+router.get("/filledTables", getFilledTables);
 
 export default router;
